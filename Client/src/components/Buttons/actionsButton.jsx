936fff23324ac8e872715e1a7d6db319bd639fc4
Farzin ahammed <farzinahammed.in@gmail.com>
2024-07-18 13:53:40 +0530

    here convert all client folders into a single folder

---
+++ Client/src/components/Buttons/actionsButton.jsx
@@ -0,0 +1,15 @@
+
+
+const ActionsButton = () => {
+    return (
+        <>
+            <div>
+                <button className="bg-[#657de7] p-1 rounded-md text-white font-semibold">
+                    Borrow
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default ActionsButton
